71197aaededcaf4d450ec840d144c47631a7845f
Emanuele Pavanello <e.pavanello@perceptolab.com>
2025-10-12 14:56:19 +0200

    shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,9 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter()
+	},
 	extensions: ['.svelte', '.svx']
 };
 
