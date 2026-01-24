3b96b8f9125742aef41113fe877cc5b7c0208c37
EmaDev <pavanello.emanuele@gmail.com>
2026-01-24 10:12:10 +0100

    ssg

--- src/routes/(auth)/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  let { children } = $props();
-</script>
-
-<div class="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-  {@render children?.()}
-</div>
